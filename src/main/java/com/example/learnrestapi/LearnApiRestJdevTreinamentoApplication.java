2acf6342dd0de8577c46980b8e623e65a66d8325
William Bigas Mauro <williambmauro@hotmail.com>
2021-06-04 09:50:36 -0300

    Initial commit

---
+++ src/main/java/com/example/learnrestapi/LearnApiRestJdevTreinamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApiRestJdevTreinamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnApiRestJdevTreinamentoApplication.class, args);
+    }
+
+}
